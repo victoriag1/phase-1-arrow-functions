b2b3eb7a681d63c9281ff0d0ceea0cdabaf8c1d5
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-07 14:29:04 -0800

    commit tree eda3779c594a258f4b6d126ec7f498e9323fd1d8

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide (){
+    return 2000/100
+}
+
+const square = function(){
+    return 2*2
+}
+
+const add = (parameter1,parameter2) => parameter1 + parameter2
+add(parameter1,parameter2);
